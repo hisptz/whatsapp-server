3c67529bb49fba63a28c8a446dd339323efa40ff
megamind <echingalo@gmail.com>
2023-04-25 16:41:37 +0300

    feat: added service for getting whatsapp groups

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,13 +1,18 @@
 export type ContactType = "individual" | "group";
-export type MessageType = 'image' | 'text' | 'file'
+export type MessageType = "image" | "text" | "file";
 
 export interface WhatsappMessage {
-    to: {
-        type: ContactType,
-        number: string;
-    }[];
-    type: MessageType;
-    text?: string;
-    image?: any;
-    file?: any;
+  to: {
+    type: ContactType;
+    number: string;
+  }[];
+  type: MessageType;
+  text?: string;
+  image?: any;
+  file?: any;
+}
+
+export interface GroupIdentifier {
+  id: string;
+  name: string;
 }
--- src/services/whatsapp.ts
+++ src/services/whatsapp.ts
@@ -1,108 +1,125 @@
-import {create, CreateOptions, Whatsapp,} from '@wppconnect-team/wppconnect';
-import {ContactType, WhatsappMessage} from "../interfaces";
-import {compact, find, isEmpty} from "lodash";
+import { create, CreateOptions, Whatsapp } from "@wppconnect-team/wppconnect";
+import { ContactType, GroupIdentifier, WhatsappMessage } from "../interfaces";
+import { compact, find, isEmpty, map } from "lodash";
 
 class WhatsappService {
-    whatsapp?: Whatsapp;
-    session: string = "whatsapp-session";
-    options?: CreateOptions
+  whatsapp?: Whatsapp;
+  session: string = "whatsapp-session";
+  options?: CreateOptions;
 
-    constructor(session?: string, options?: CreateOptions) {
-        this.options = options;
-        if (session) {
-            this.session = session;
+  constructor(session?: string, options?: CreateOptions) {
+    this.options = options;
+    if (session) {
+      this.session = session;
+    }
+    this.getGroupChatId = this.getGroupChatId.bind(this);
+    this.getContactChatId = this.getContactChatId.bind(this);
+    this.getChatId = this.getChatId.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.sendImageMessage = this.sendImageMessage.bind(this);
+    this.sendTextMessage = this.sendTextMessage.bind(this);
+  }
+
+  get client() {
+    if (this.whatsapp) {
+      return this.whatsapp;
+    } else {
+      throw "Client not initialized";
+    }
+  }
+
+  /*
+   * Initializes whatsapp instance
+   * */
+  async init() {
+    this.whatsapp = await create({
+      session: this.session,
+      ...(this.options ?? {}),
+    });
+    return true;
+  }
+
+  async getAllGroups(): Promise<GroupIdentifier[]> {
+    try {
+      const groups = await this.client.getAllGroups();
+
+      return map(groups, (group) => ({
+        id: group.id._serialized,
+        name: group.name,
+      }));
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getGroupChatId(groupName: string): Promise<string> {
+    const groups = await this.client.getAllGroups();
+    if (isEmpty(groups)) throw `There are no groups for this phone number`;
+
+    const group = find(groups, (group) => {
+      return group.name === groupName;
+    });
+
+    if (isEmpty(group)) throw `Group ${groupName} not found`;
+
+    return group.id._serialized;
+  }
+
+  async getContactChatId(number: string) {
+    return `${number}@c.us`;
+  }
+
+  async getChatId({ type, number }: { type: ContactType; number: string }) {
+    switch (type) {
+      case "individual":
+        return this.getContactChatId(number);
+      case "group":
+        return this.getGroupChatId(number);
+    }
+  }
+
+  async getChatIds(
+    contacts: { type: ContactType; number: string }[]
+  ): Promise<Awaited<string>[]> {
+    return Promise.all(contacts.map(this.getChatId));
+  }
+
+  async sendMessage(message: WhatsappMessage) {
+    const { text, to, type, image } = message;
+    const chatIds = compact(await this.getChatIds(to));
+
+    if (isEmpty(chatIds)) {
+      throw `Error sending message. Could not get chat id`;
+    }
+
+    switch (type) {
+      case "image":
+        return Promise.all(
+          chatIds.map((chatId) => this.sendImageMessage(chatId, image, text))
+        );
+      case "text":
+        if (!text) {
+          throw `Please specify text to send`;
         }
-        this.getGroupChatId = this.getGroupChatId.bind(this);
-        this.getContactChatId = this.getContactChatId.bind(this);
-        this.getChatId = this.getChatId.bind(this);
-        this.sendMessage = this.sendMessage.bind(this);
-        this.sendImageMessage = this.sendImageMessage.bind(this);
-        this.sendTextMessage = this.sendTextMessage.bind(this);
-
+        return Promise.all(
+          chatIds.map((chatId) => this.sendTextMessage(chatId, text))
+        );
+      default:
+        throw `Sending ${type}  messages is currently not supported`;
     }
+  }
 
-    get client() {
-        if (this.whatsapp) {
-            return this.whatsapp;
-        } else {
-            throw  "Client not initialized"
-        }
-    }
+  protected async sendImageMessage(
+    chatId: string,
+    imagePath: string,
+    caption?: string
+  ) {
+    return this.client.sendImage(`${chatId}`, imagePath, undefined, caption);
+  }
 
-    /*
-    * Initializes whatsapp instance
-    * */
-    async init() {
-        this.whatsapp = await create({
-            session: this.session,
-            ...(this.options ?? {})
-        });
-        return true;
-    }
-
-    async getGroupChatId(groupName: string): Promise<string> {
-        const groups = await this.client.getAllGroups();
-        if (isEmpty(groups)) throw `There are no groups for this phone number`;
-
-        const group = find(groups, (group) => {
-            return group.name === groupName
-        })
-
-        if (isEmpty(group)) throw `Group ${groupName} not found`
-
-        return group.id._serialized;
-    }
-
-    async getContactChatId(number: string) {
-        return `${number}@c.us`;
-    }
-
-    async getChatId({type, number}: { type: ContactType; number: string }) {
-        switch (type) {
-            case "individual":
-                return this.getContactChatId(number);
-            case "group":
-                return this.getGroupChatId(number)
-        }
-    }
-
-    async getChatIds(contacts: { type: ContactType; number: string }[]): Promise<Awaited<string>[]> {
-        return Promise.all(contacts.map(this.getChatId));
-    }
-
-    async sendMessage(message: WhatsappMessage) {
-        const {text, to, type, image} = message;
-        const chatIds = compact(await this.getChatIds(to));
-
-        if (isEmpty(chatIds)) {
-            throw `Error sending message. Could not get chat id`
-        }
-
-        switch (type) {
-            case "image":
-                return Promise.all(chatIds.map((chatId) => this.sendImageMessage(chatId, image, text)))
-            case "text":
-                if (!text) {
-                    throw `Please specify text to send`
-                }
-                return Promise.all(chatIds.map((chatId) => this.sendTextMessage(chatId, text)));
-            default:
-                throw `Sending ${type}  messages is currently not supported`
-        }
-
-
-    }
-
-    protected async sendImageMessage(chatId: string, imagePath: string, caption?: string) {
-        return this.client.sendImage(`${chatId}`, imagePath, undefined, caption)
-    }
-
-    protected async sendTextMessage(chatId: string, message: string) {
-        return this.client.sendText(`${chatId}`, message)
-    }
-
-
+  protected async sendTextMessage(chatId: string, message: string) {
+    return this.client.sendText(`${chatId}`, message);
+  }
 }
-
 
 export default new WhatsappService();
