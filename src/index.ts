667c2482b502b10a3b9a55e44786cbf3594cc950
gian <nnkogift@hotmail.com>
2023-05-17 18:25:42 +0300

    fix(express): Fixes
    - Fixed payload size limit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { config as configEnv } from "dotenv";
+import {config as configEnv} from "dotenv";
 import express from "express";
 import routes from "./routes";
 import bodyParser from "body-parser";
@@ -7,17 +7,17 @@ import Whatsapp from "./services/whatsapp";
 configEnv();
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: "50mb"}));
 const port = process.env.PORT ?? 4000;
 
 app.use(`/`, routes);
 
 Whatsapp.init()
-  .then((status) => {
-    app.listen(port, () => {
-      console.info(`Whatsapp server started at port ${port}`);
+    .then((status) => {
+        app.listen(port, () => {
+            console.info(`Whatsapp server started at port ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error(`Could not start whatsapp server: ${error}`);
     });
-  })
-  .catch((error) => {
-    console.error(`Could not start whatsapp server: ${error}`);
-  });
