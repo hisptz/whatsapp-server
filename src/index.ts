8401b047f740f0095769407c4626b2fddb7658ac
megamind <echingalo@gmail.com>
2023-07-14 11:17:33 +0300

    feat(src/index.ts): added rate limiter for 100 requests per minute

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,30 @@ import routes from "./routes";
 import bodyParser from "body-parser";
 import Whatsapp from "./services/whatsapp";
 import helmet from "helmet";
+import RateLimit from "express-rate-limit";
 
 configEnv();
 
 const app = express();
 
 app.use(apiKeyAuth(/^API_KEY/));
+
 app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
   })
 );
+
+const limiter = RateLimit({
+  windowMs: 10 * 1000,
+  max: 100,
+  handler: (req, res) => {
+    res
+      .status(429)
+      .json({ error: "Too many requests, please try again later." });
+  },
+});
+app.use(limiter);
 
 app.use(bodyParser.json({ limit: "50mb" }));
 const port = process.env.PORT ?? 4000;
@@ -26,7 +39,7 @@ app.use(`/`, routes);
 Whatsapp.init()
   .then((status) => {
     app.listen(port, () => {
-      console.info(`Whatsapp server started at port ${port}`);
+      console.info(`Whatsapp server started at port ${port}s`);
     });
   })
   .catch((error) => {
