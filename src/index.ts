56bc9c99219f1715ee7eae53e1f48e740538b85e
megamind <echingalo@gmail.com>
2023-07-14 10:53:28 +0300

    refactor(src/index.ts): added support for the api key authentification

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,34 @@
-import {config as configEnv} from "dotenv";
+import { config as configEnv } from "dotenv";
+import { apiKeyAuth } from "@vpriem/express-api-key-auth";
+
 import express from "express";
 import routes from "./routes";
 import bodyParser from "body-parser";
 import Whatsapp from "./services/whatsapp";
+import helmet from "helmet";
 
 configEnv();
 
 const app = express();
-app.use(bodyParser.json({limit: "50mb"}));
+
+app.use(apiKeyAuth(/^API_KEY/));
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+  })
+);
+
+app.use(bodyParser.json({ limit: "50mb" }));
 const port = process.env.PORT ?? 4000;
 
 app.use(`/`, routes);
 
 Whatsapp.init()
-    .then((status) => {
-        app.listen(port, () => {
-            console.info(`Whatsapp server started at port ${port}`);
-        });
-    })
-    .catch((error) => {
-        console.error(`Could not start whatsapp server: ${error}`);
+  .then((status) => {
+    app.listen(port, () => {
+      console.info(`Whatsapp server started at port ${port}`);
     });
+  })
+  .catch((error) => {
+    console.error(`Could not start whatsapp server: ${error}`);
+  });
